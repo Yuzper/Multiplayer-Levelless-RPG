43f447cd80f8cdd802c45dedf3772dca9347a326
Sebastian <sebsthiel@gmail.com>
2024-03-30 17:27:26 +0100

    Added frostbold

--- Assets/ProjectileDamageCollider.cs
+++ Assets/ProjectileDamageCollider.cs
@@ -6,7 +6,20 @@ public class ProjectileDamageCollider : DamageCollider
 {
     protected override void OnTriggerEnter(Collider other)
     {
-        base.OnTriggerEnter(other);
-        Destroy(gameObject);
+        CharacterManager damageTarget = other.GetComponentInParent<CharacterManager>();
+
+        if (damageTarget != null)
+        {
+            contactPoint = other.gameObject.GetComponent<Collider>().ClosestPointOnBounds(transform.position);
+
+            // Check if we can damage this target based on friendly fire
+            if (WorldUtilityManager.instance.CanIDamageThisTarget(characterCausingDamage.characterGroup, damageTarget.characterGroup))
+            {
+                // Check if target is blocking
+
+                DamageTarget(damageTarget);
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
--- Assets/SpellSystem/BaseSpell.cs
+++ Assets/SpellSystem/BaseSpell.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Spellsystem/Spells/BaseSpell")]
 public class BaseSpell : ScriptableObject
 {
     public string spellName;
